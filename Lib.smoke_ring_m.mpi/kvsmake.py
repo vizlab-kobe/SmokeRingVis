b735dfdd9eb1dda0e0600915e3f4dd5384d27dcb
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2023-05-29 12:16:31 +0900

    Modified.

--- Lib.smoke_ring_m.mpi/kvsmake.py
+++ Lib.smoke_ring_m.mpi/kvsmake.py
@@ -14,7 +14,7 @@ def KVSMake( option ):
     if os.name == 'nt': s = "&"
 
     make_option = ''
-    if option == 'build': make_option = "kvsmake lib" + s
+    if   option == 'build': make_option = "kvsmake lib" + s
     elif option == 'clean': make_option = "kvsmake clean" + s
     elif option == 'distclean': make_option = "kvsmake distclean" + s
     elif option == 'rebuild': make_option = "kvsmake clean" + s + "kvsmake lib" + s
@@ -30,11 +30,63 @@ def KVSMake( option ):
     command += make_option
     os.system( command )
 
+def MakeSliceSVG( option ):
+
+    s = ";"
+    if os.name == 'nt': s = "&"
+
+    make_option = ''
+    if   option == 'build': make_option = "make"
+    elif option == 'clean': make_option = "make clean"
+    elif option == 'distclean': make_option = "make clean"
+    elif option == 'rebuild': make_option = "make clean" + s + "make"
+    elif option == 'debug_build': make_option = "make" + s
+    elif option == 'debug_rebuild': make_option = "make clean" + s + "make"
+    else:
+        print( "Error: Unknown option '" + option +"'" )
+        print( "Usage: python kvsmake.py [clean | distclean | rebuild]" )
+        sys.exit()
+
+    command = "cd slice-svg/src/" + s
+    command += make_option
+    os.system( command )
+
+def MakeSmokeRing( option ):
+
+    s = ";"
+    if os.name == 'nt': s = "&"
+
+    make_option = ''
+    if   option == 'build': make_option = "make"
+    elif option == 'clean': make_option = "make clean"
+    elif option == 'distclean': make_option = "make clean"
+    elif option == 'rebuild': make_option = "make clean" + s + "make"
+    elif option == 'debug_build': make_option = "make" + s
+    elif option == 'debug_rebuild': make_option = "make clean" + s + "make"
+    else:
+        print( "Error: Unknown option '" + option +"'" )
+        print( "Usage: python kvsmake.py [clean | distclean | rebuild]" )
+        sys.exit()
+
+    command = "cd smoke-ring/src/" + s
+    command += make_option
+    if 'build' in option: command += s + "cp *.o *.mod ../.." + s + "rm ../../main.o" + s
+    os.system( command )
+
 #=============================================================================
 #  Main process.
 #=============================================================================
 if __name__=='__main__':
     argc = len( sys.argv )
     argv = sys.argv
-    if   argc == 1: KVSMake( 'build' )
-    elif argc == 2: KVSMake( argv[1] )
+    if   argc == 1:
+        MakeSliceSVG( 'build' )
+        MakeSmokeRing( 'build' )
+        KVSMake( 'build' )
+    elif argc == 2:
+        MakeSliceSVG( argv[1] )
+        MakeSmokeRing( argv[1] )
+        KVSMake( argv[1] )
+
+#    if   argc == 1: KVSMake( 'build' )
+#    elif argc == 2: KVSMake( argv[1] )
