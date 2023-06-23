9710583e27b55eeb101343e84deb6793fa00f0f0
Matsushima <matsushima@s168.lab>
2023-06-23 09:50:23 +0900

    modified

--- Test/SmokeRing.mpi/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PS_COUNT=`ps aux | grep SmokeRing.mpi | grep -v grep | wc -l`
-if [ $PS_COUNT -gt 0 ]; then
-    killall SmokeRing.mpi
-fi
-
-if [ -e "Makefile.kvs" ]; then
-    kvsmake distclean
-fi
--- Test/SmokeRing/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-kvsmake distclean
-
