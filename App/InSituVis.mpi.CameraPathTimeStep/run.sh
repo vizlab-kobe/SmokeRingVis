dcdae58cf6340c59406ab0182983a4e444ea4d4b
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2026-06-04 18:25:18 +0900

    Improve run scripts: macOS stack & MPI options
    Raise the stack size on macOS in run.sh to avoid segmentation faults from large local arrays. For MPI variants, add configurable MPIEXEC and MPI_OPTIONS, auto-detect Open MPI to enable --oversubscribe when MPI_OPTIONS is not set, and use these to launch mpiexec -np NPROCS. Applied to all InSituVis run scripts to improve portability and allow oversubscribing on Open MPI.

--- App/InSituVis.mpi.CameraPathTimeStep/run.sh
+++ App/InSituVis.mpi.CameraPathTimeStep/run.sh
@@ -2,6 +2,14 @@
 PROGRAM=${PWD##*/}
 PARAMS_FILE="params.namelist"
 
+# macOS: raise stack size to avoid segmentation faults from large local arrays.
+if [ "$(uname -s)" = "Darwin" ]; then
+    HARD_STACK=`ulimit -H -s 2>/dev/null`
+    if [ -n "$HARD_STACK" ]; then
+        ulimit -S -s "$HARD_STACK" 2>/dev/null || true
+    fi
+fi
+
 DATA_DIR=`cat $PARAMS_FILE | grep 'Data_dir_name' | awk '{print substr($4, 2, length($4)-2)}'`
 RESTART_DIR="restart"
 VIS2D_DIR="vis2d"
@@ -17,4 +25,12 @@ NPROC_Y=`cat ./make.sh | grep '^NPROC_Y=' | awk -F "=" '{print $2}' | awk '{prin
 NPROC_Z=`cat ./make.sh | grep '^NPROC_Z=' | awk -F "=" '{print $2}' | awk '{print $1}'`
 NPROCS=`echo "$NPROC_X * $NPROC_Y * $NPROC_Z" | bc`
 
-mpiexec -np $NPROCS ./$PROGRAM $PARAMS_FILE
+MPIEXEC=${MPIEXEC:-mpiexec}
+MPI_OPTIONS=${MPI_OPTIONS:-}
+
+# Open MPI: allow running more MPI ranks than detected slots.
+if [ -z "$MPI_OPTIONS" ] && $MPIEXEC --version 2>/dev/null | grep -qi "Open MPI"; then
+    MPI_OPTIONS="--oversubscribe"
+fi
+
+$MPIEXEC $MPI_OPTIONS -np "$NPROCS" ./"$PROGRAM" "$PARAMS_FILE"
--- App/InSituVis/run.sh
+++ App/InSituVis/run.sh
@@ -2,4 +2,12 @@
 PROGRAM=${PWD##*/}
 PARAMS_FILE="params.namelist"
 
+# macOS: raise stack size to avoid segmentation faults from large local arrays.
+if [ "$(uname -s)" = "Darwin" ]; then
+    HARD_STACK=`ulimit -H -s 2>/dev/null`
+    if [ -n "$HARD_STACK" ]; then
+        ulimit -S -s "$HARD_STACK" 2>/dev/null || true
+    fi
+fi
+
 ./$PROGRAM ${PARAMS_FILE} 1
